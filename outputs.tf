f7df06b66554e59b2964f26279f2d73999907bcc
Sean Sundberg <seansund@us.ibm.com>
2021-04-13 00:27:33 -0500

    Updates outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "subnet_count" {
+output "count" {
   description = "The number of subnets created"
-  value       = var.subnet_count
+  value       = var._count
 }
 
 output "subnets" {
   description = "The subnets that were created"
-  value       = [ for subnet in ibm_is_subnet.vpc_subnet: {id = subnet.id, zone = subnet.zone, label = var.subnet_label} ]
+  value       = [ for subnet in ibm_is_subnet.vpc_subnet: {id = subnet.id, zone = subnet.zone, label = var.label} ]
 }
