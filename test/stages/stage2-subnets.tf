b91890f55fab8e9b9ac2d102871b60b2ed87d4c8
Sean Sundberg <seansund@us.ibm.com>
2021-04-13 00:24:16 -0500

    Updates gateways variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-subnets.tf
+++ test/stages/stage2-subnets.tf
@@ -6,7 +6,7 @@ module "subnets" {
   ibmcloud_api_key  = var.ibmcloud_api_key
   vpc_name          = module.vpc.name
   acl_id            = module.vpc.acl_id
-  gateway_ids       = module.gateways.ids
+  gateways          = module.gateways.gateways
   _count            = var.vpc_subnet_count
   label             = "cluster"
 }
