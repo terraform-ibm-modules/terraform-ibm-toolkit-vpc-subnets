d5c57e1cf102eafd575a7e12a82e27b61d93ca7a
Sean Sundberg <seansund@us.ibm.com>
2021-04-26 09:18:16 -0500

    Spits address count and cidr_block configurations
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ locals {
   zone_count         = 3
   vpc_zone_names     = [ for index in range(var._count): "${var.region}-${(index % local.zone_count) + 1}" ]
   gateway_count      = min(length(var.gateways), local.zone_count)
-  ipv4_cidr_block    = length(var.ipv4_cidr_blocks) >= var._count ? var.ipv4_cidr_blocks : [ for index in range(var._count): "" ]
-  ipv4_address_count = length(var.ipv4_cidr_blocks) >= var._count ? "" : var.ipv4_address_count
+  ipv4_cidr_provided = length(var.ipv4_cidr_blocks) >= var._count
+  ipv4_cidr_block    = local.ipv4_cidr_provided ? var.ipv4_cidr_blocks : [ for index in range(var._count): "" ]
+  ipv4_address_count = local.ipv4_cidr_provided ? "" : var.ipv4_address_count
 }
 
 resource null_resource print_names {
@@ -14,15 +15,6 @@ resource null_resource print_names {
   provisioner "local-exec" {
     command = "echo 'VPC name: ${var.vpc_name}'"
   }
-}
-
-data ibm_is_vpc vpc {
-  depends_on = [null_resource.print_names]
-
-  name  = var.vpc_name
-}
-
-resource null_resource print-values {
   provisioner "local-exec" {
     command = "echo 'IPv4 address count: ${local.ipv4_address_count}'"
   }
@@ -31,8 +23,14 @@ resource null_resource print-values {
   }
 }
 
-resource ibm_is_subnet vpc_subnet {
-  count                    = var._count
+data ibm_is_vpc vpc {
+  depends_on = [null_resource.print_names]
+
+  name  = var.vpc_name
+}
+
+resource ibm_is_subnet vpc_subnet_cidr_block {
+  count                    = local.ipv4_cidr_provided ? var._count : 0
 
   name                     = "${var.vpc_name}-subnet-${var.label}${format("%02s", count.index)}"
   zone                     = local.vpc_zone_names[count.index]
@@ -42,4 +40,16 @@ resource ibm_is_subnet vpc_subnet {
   resource_group           = var.resource_group_id
   network_acl              = var.acl_id
   ipv4_cidr_block          = local.ipv4_cidr_block[count.index]
+}
+
+resource ibm_is_subnet vpc_subnet_total_count {
+  count                    = local.ipv4_cidr_provided ? 0 : var._count
+
+  name                     = "${var.vpc_name}-subnet-${var.label}${format("%02s", count.index)}"
+  zone                     = local.vpc_zone_names[count.index]
+  vpc                      = data.ibm_is_vpc.vpc.id
+  public_gateway           = coalesce([ for gateway in var.gateways: gateway.id if gateway.zone == local.vpc_zone_names[count.index] ]...)
+  total_ipv4_address_count = local.ipv4_address_count
+  resource_group           = var.resource_group_id
+  network_acl              = var.acl_id
 }
