a28c9a931f17e65313ea2cc281af8e5f5fa86ec5
Andrew Trice <amtrice@us.ibm.com>
2021-05-05 00:12:30 -0400

    updated subnet iteration for flow logs
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -89,11 +89,11 @@ data ibm_is_subnet vpc_subnet {
 }
 
 resource ibm_is_flow_log flowlog_instance {
-  count = (var.flow_log_cos_bucket_name != "") ? var._count : 0
+  count = (var.flow_log_cos_bucket_name != "" && var.provision && !local.ipv4_cidr_provided) ? var._count : 0
 
   name = "${local.name_prefix}${format("%02s", count.index)}-flowlog"
   active = true
-  target = data.ibm_is_subnet.vpc_subnet[count.index].id
+  target = ibm_is_subnet.vpc_subnet_total_count[count.index].id
   resource_group = var.resource_group_id
   storage_bucket = var.flow_log_cos_bucket_name
 }
