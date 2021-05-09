c8c60ccf4754868d371b2e272dea7c564813c18c
Sean Sundberg <seansund@us.ibm.com>
2021-05-09 16:48:10 -0500

    Updates test inputs for ipv4_cidr_blocks
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-subnets.tf
+++ test/stages/stage2-subnets.tf
@@ -8,7 +8,7 @@ module "subnets" {
   gateways           = module.gateways.gateways
   _count             = var.vpc_subnet_count
   label              = "cluster"
-  ipv4_cidr_blocks   = split(",", var.ipv4_cidr_blocks)
+  ipv4_cidr_blocks   = jsondecode(var.ipv4_cidr_blocks)
   ipv4_address_count = var.ipv4_address_count
   auth_id             = module.flow-log-auth.id
   flow_log_cos_bucket_name = module.dev_cos_bucket.bucket_name
