00744c37ab666fdaf1d6f03f60cc8cc108e8146f
Andrew Trice <amtrice@us.ibm.com>
2021-11-22 13:01:53 -0500

    updated tests
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage1-vpc.tf
+++ test/stages/stage1-vpc.tf
@@ -5,6 +5,4 @@ module "vpc" {
   resource_group_name = module.resource_group.name
   region              = var.region
   name_prefix         = var.name_prefix
-  address_prefix_count = var.address_prefix_count
-  address_prefixes = jsondecode(var.address_prefixes)
 }
