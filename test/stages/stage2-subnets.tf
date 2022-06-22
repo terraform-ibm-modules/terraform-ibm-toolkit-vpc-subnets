b7e35c0dea4edbcac9050ff6cba97af19ccc9f8a
Andrew Trice <amtrice@us.ibm.com>
2022-06-22 15:34:15 -0400

    refactored name prefix in test
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-subnets.tf
+++ test/stages/stage2-subnets.tf
@@ -1,7 +1,6 @@
 module "subnets" {
   source = "./module"
 
-  name_prefix        = "testing-prefix"
   resource_group_name = module.resource_group.name
   region             = var.region
   vpc_name           = module.vpc.name
